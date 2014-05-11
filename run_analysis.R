d49e45a6cb012bf1bc007e01ad5c5a492d15a4cc
totocol <totocol@yahoo.com>
2014-05-11 19:56:02 +0700

    Finished script

--- run_analysis.R
+++ run_analysis.R
@@ -71,4 +71,93 @@ selectedData <- subset(datawithLabels, select = c(1:8))
 
 #Now we need to calculate the averages 
 
-head(selectedData)
+#let's sort the table by subject and then by activity
+
+orderSelected <- selectedData[order(selectedData[,2], selectedData$activities, na.last=NA),]
+
+#Initialise data frame
+
+dataAverages <- data.frame("tbodyacc.mean.x"=as.numeric(), "tbodyacc.mean.y"=as.numeric(), 
+                           "tbodyacc.mean.x"=as.numeric(),"tgravityacc.mean.x"=as.numeric(),
+                           "tgravityacc.mean.y"=as.numeric(),"tgravityacc.mean.z"=as.numeric())
+
+#And now lets calculate the average for each subject on each activity
+
+#For laying
+for (i in seq_along(1:30)) {
+  layingSubset <- subset(orderSelected, orderSelected$activities=="laying")
+  layingColumns <- subset(layingSubset, subjects == i, select = tbodyacc.mean.x:tgravityacc.mean.z)  
+  dataAverages <- rbind(dataAverages, colMeans(layingColumns[,1:6]))
+}
+
+#For sitting
+
+for (i in seq_along(1:30)) {
+  sittingSubset <- subset(orderSelected, orderSelected$activities=="sitting")
+  sittingColumns <- subset(layingSubset, subjects == i, select = tbodyacc.mean.x:tgravityacc.mean.z)  
+  dataAverages <- rbind(dataAverages, colMeans(sittingColumns[,1:6]))
+}
+
+
+#For standing 
+
+for (i in seq_along(1:30)) {
+  standingSubset <- subset(orderSelected, orderSelected$activities=="standing")
+  standingColumns <- subset(layingSubset, subjects == i, select = tbodyacc.mean.x:tgravityacc.mean.z)  
+  dataAverages <- rbind(dataAverages, colMeans(standingColumns[,1:6]))
+}
+
+#For walking 
+for (i in seq_along(1:30)) {
+  walkingSubset <- subset(orderSelected, orderSelected$activities=="walking")
+  walkingColumns <- subset(layingSubset, subjects == i, select = tbodyacc.mean.x:tgravityacc.mean.z)  
+  dataAverages <- rbind(dataAverages, colMeans(walkingColumns[,1:6]))
+}
+
+colnames(dataAverages) <- c("tbodyacc.mean.x", "tbodyacc.mean.y", 
+                            "tbodyacc.mean.x","tgravityacc.mean.x",
+                            "tgravityacc.mean.y","tgravityacc.mean.z")
+
+#For walking downstairs
+for (i in seq_along(1:30)) {
+  walking.downstairsSubset <- subset(orderSelected, orderSelected$activities=="walking.downstairs")
+  walking.downstairsColumns <- subset(layingSubset, subjects == i, select = tbodyacc.mean.x:tgravityacc.mean.z)  
+  dataAverages <- rbind(dataAverages, colMeans(walking.downstairsColumns[,1:6]))
+}
+
+#For walking upstaris
+for (i in seq_along(1:30)) {
+  walking.upstairsSubset <- subset(orderSelected, orderSelected$activities=="walking.upstairs")
+  walking.upstairsColumns <- subset(layingSubset, subjects == i, select = tbodyacc.mean.x:tgravityacc.mean.z)  
+  dataAverages <- rbind(dataAverages, colMeans(walking.upstairsColumns[,1:6]))
+}
+
+#And the final result
+
+
+
+#recreate activity names
+finalLaying <- as.data.frame(rep("laying", 30))
+colnames(finalLaying) = "activities"
+finalSitting <- as.data.frame(rep("sitting", 30))
+colnames(finalSitting) = "activities"
+finalStanding <- as.data.frame(rep("standing", 30))
+colnames(finalStanding) = "activities"
+finalWalking <- as.data.frame(rep("walking", 30))
+colnames(finalWalking) = "activities"
+finalDonwstairs <- as.data.frame(rep("walking.donwstairs", 30))
+colnames(finalDonwstairs) = "activities"
+finalUpstairs <- as.data.frame(rep("walking.upstairs", 30))
+colnames(finalUpstairs) = "activities"
+
+finalActivities <- rbind(finalLaying,finalSitting,finalStanding,finalWalking,finalDonwstairs,finalUpstairs)
+
+
+finalSubjects <- as.data.frame(paste("subject", 1:30, sep = "."))
+subjectAll <- rbind(finalSubjects, finalSubjects, finalSubjects, finalSubjects, finalSubjects, finalSubjects)
+colnames(subjectAll) <- "subject.number"
+
+finalDataset <- data.frame(finalActivities, subjectAll, dataAverages)
+
+write.table(finalDataset, file="cleanaveragescoursera.csv", sep = ",", row.names=FALSE)
+
